0c67b164ca73af0d3499082f6efe8d41ebdc71db
guibersi <guilherme.bersi@skeelo.com>
2024-12-23 03:03:11 -0300

    feat: adding website tab title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,12 @@ import { CTA } from "@/components/CTA";
 import { Features } from "@/components/Features";
 import { Hero } from "@/components/Hero";
 import { Navbar } from "@/components/Navbar";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Modern Dev Tools",
+  description: "...",
+};
 
 export default function Home() {
   return (
